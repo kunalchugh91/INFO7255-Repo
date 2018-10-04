5191c3f99b6c94450bf6655b451abd0563ab5609
chugh.k <chugh.k@husky.neu.edu>
2018-10-04 17:49:27 -0400

    Added beans, post mapping method

---
+++ src/main/java/com/example/beans/JedisBean.java
@@ -0,0 +1,80 @@
+package com.example.beans;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisException;
+
+public class JedisBean {
+
+	private static final String redisHost = "localhost";
+	private static final Integer redisPort = 6379;
+	private static JedisPool pool = null;
+	
+	public JedisBean() {
+		pool = new JedisPool(redisHost, redisPort);
+	}
+	
+	public UUID insert(JSONObject jsonObject) {
+		UUID idOne = UUID.randomUUID();
+		if(insertUtil(jsonObject, idOne))
+			return idOne;
+		else
+			return null;
+	}
+	
+	private boolean insertUtil(JSONObject jsonObject, UUID uuid) {
+		
+		try {
+			Jedis jedis = pool.getResource();
+			
+			for(Object key : jsonObject.keySet()) {
+				String attributeKey = uuid + "_" + String.valueOf(key);
+				Object attributeVal = jsonObject.get(String.valueOf(key));
+				
+				if(attributeVal instanceof JSONObject) {
+					Map<String,String> map = handleObjectAsMap( (JSONObject) attributeVal);
+					jedis.hmset(attributeKey, map);
+				} else if (attributeVal instanceof JSONArray) {
+					Set<String> set  = handleObjectAsArray((JSONArray) attributeVal);
+					for(String v : set)
+						jedis.sadd(attributeKey, v);
+				} else {
+					jedis.set(attributeKey, String.valueOf(attributeVal));
+				}
+			}
+		}
+		catch(JedisException e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private Map<String,String> handleObjectAsMap(JSONObject jsonObject) {
+		Map<String,String> map = new HashMap<String, String>();
+		for(String key : jsonObject.keySet()) {
+			map.put(key, jsonObject.get(key).toString());
+		}
+		return map;
+	}
+	
+	private Set<String> handleObjectAsArray(JSONArray jsonArray) {
+		Set<String> set = new HashSet<String>();
+		for(Object o : jsonArray) {
+			JSONObject ob = (JSONObject) o;
+			set.add(ob.toString());
+		}
+		return set;
+	}
+	
+}	
+
--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,16 +5,27 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 //import java.util.ArrayList;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.beans.JedisBean;
+import com.example.beans.MyJsonValidator;
+
+import redis.clients.jedis.JedisPool;
 
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.ValidationException;
@@ -25,52 +36,72 @@ import org.json.JSONTokener;
 @RestController
 public class HomeController {
 	
-	//@Autowired
-	//private NameConverterBean nameConverter ;
+	@Autowired
+	private MyJsonValidator validator;
+	@Autowired
+	private JedisBean jedisBean;
 	
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome!";
+	}
+
+	// to read json instance from redis
+	@GetMapping("/read/{id}")
+	public ResponseEntity<String> read(@PathVariable(name="id", required=true) String id) {
+		return new ResponseEntity<String>("invalid", HttpStatus.BAD_REQUEST);
+	}
+	
+	
+	// to insert new json instance into redis
 	@PostMapping("/insert")
-	public ResponseEntity<String> insert(@RequestBody String body) {
-		if(body == null) {
-			return new ResponseEntity<String>("no json body", HttpStatus.BAD_REQUEST);
-		}
+	public ResponseEntity<String> insert(@RequestBody(required=true) String body) {
 		
-		Schema schema = getSchema();
+		Schema schema = validator.getSchema();
 		if(schema == null)
-			return new ResponseEntity<String>("file not found exception", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>("schema file not found exception", HttpStatus.BAD_REQUEST);
 		
-		try {
-			JSONObject jsonInstance = getJsonObject(body);
-			schema.validate(jsonInstance);
-			}
-		catch (ValidationException e) {
+		JSONObject jsonObject = validator.getJsonObjectFromString(body);
+		
+		if(validator.validate(jsonObject)) {
+			UUID uuid = jedisBean.insert(jsonObject);
+			return new ResponseEntity<String>("Inserted with id "+uuid, HttpStatus.ACCEPTED);
+		}
+		else {
 			return new ResponseEntity<String>("invalid", HttpStatus.BAD_REQUEST);
 		}
-		
-		return new ResponseEntity<String>("valid", HttpStatus.ACCEPTED);
+			
 	}
 	
-	public static Schema getSchema() {
-		
-	    File file = new File("src/main/resources/schema.json");
-	    
-	    InputStream inputStream;
-		try {
-			inputStream = new FileInputStream(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-	    
-	    JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
-	    Schema schema = SchemaLoader.load(rawSchema);
-	    return schema;
-		
+	
+	// to delete json instance with key id from redis
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable(name="id", required=true) String id) {
+		return new ResponseEntity<String>("invalid", HttpStatus.BAD_REQUEST);
 	}
 	
-	public static JSONObject getJsonObject(String json) {
+	
+	// to update json instance with key id in redis
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> update(@PathVariable(name="id", required=true) String id, @RequestBody(required=true) String body) {
 		
-	    return new JSONObject(json);
+		//if id does not exist
+		if(id)
+		return insert(body);
+		
+		//else
+		Schema schema = validator.getSchema();
+		if(schema == null)
+			return new ResponseEntity<String>("schema file not found exception", HttpStatus.BAD_REQUEST);
+		
+		JSONObject jsonObject = validator.getJsonObjectFromString(body);
+		
+		if(validator.validate(jsonObject))
+			return new ResponseEntity<String>("valid", HttpStatus.ACCEPTED);
+		
+		else
+			return new ResponseEntity<String>("invalid", HttpStatus.BAD_REQUEST);
 	}
 
 
